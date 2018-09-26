a9541391dfcd94231f66f0fdd3b0e45750e0294e
Dev Salman <dabbakutisalman@gmail.com>
2018-09-26 22:48:50 +0530

    added new functions

--- basic.go
+++ basic.go
@@ -6,6 +6,10 @@ comment */
 package main
 import "fmt"
 import "time"
+import "math"
+func add(x,y int) int {
+    return (x + y)
+}
 func main() {
     evennums := [5] int {2,4,6,8,10}
     age := 16
@@ -21,6 +25,7 @@ func main() {
     fmt.Println("x is" ,x)
     fmt.Println("y is" ,y)
     fmt.Println("pi is" ,pi)
+    fmt.Println(math.Pi)
     fmt.Println("Developer Name:",Name)
     fmt.Println("a,b=",a, b)
     fmt.Println("a+b=" ,a+b)
@@ -30,6 +35,8 @@ func main() {
     fmt.Println("a/b=" ,a/b)
     fmt.Println(!hi)
     fmt.Println(isbool)
+    result:= add(45,55)
+    fmt.Println("result for add=",result)
     for i:=1; i<=5; i++ {
         fmt.Println("passed" ,i)
      }
@@ -39,7 +46,7 @@ func main() {
     }
     switch age {
         case 16: fmt.Println("prepare for college")
-        case 18: fmt.Println("Dont run after girls")
+        case 18: fmt.Println("Don't run after girls")
         case 20: fmt.Println("get yourself a job")
         default: fmt.Println("are you still alive")                            
     }

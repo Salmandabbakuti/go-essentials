c42d8c2ea51bf090aad8632a1b437d5dace2ee98
Dev Salman <dabbakutisalman@gmail.com>
2018-10-04 19:37:36 +0530

    added structs

--- AtoZ.go
+++ AtoZ.go
@@ -10,7 +10,7 @@ import "math"
 import "math/rand"
 func add(x,y int) int {
     return (x + y)
-}
+         }
 func main() {
     evennums := [5] int {2,4,6,8,10}
     age := 20
@@ -39,25 +39,35 @@ func main() {
     fmt.Println(isbool)
     result:= add(45,55)
     fmt.Println("result for add=",result)
-    Salary := make(map[string]int)
-    Salary["Rajesh"] = 12000
-    Salary["Pavan"] = 15000
-    Salary["Gopi"] = 9000
-    fmt.Println("Salaries map contents:",Salary)
-    for i:=1; i<=5; i++ {
+     for i:=1; i<=5; i++ {
         fmt.Println("passed" ,i)
-     }
+             }
     if age< 18 {
         fmt.Println("No, you  cant vote")} else {
         fmt.Println("Yes,you can vote")
-    }
+              }
     switch age {
         case 16: fmt.Println("prepare for college")
         case 18: fmt.Println("Don't run after girls")
         case 20: fmt.Println("get yourself a job")
         default: fmt.Println("are you still alive")                            
-    }
-    fmt.Println(evennums[3])
-}
+              }
+    type Developers struct {
+	         Name string
+	         SalaryUSD int
+                  }
+    fmt.Println(Developers{"Salman" , 6000})
+	fmt.Println(Developers{"Bhemesh" , 6000})
+    fmt.Println(Developers{"Prena" , 5500})
+	fmt.Println(Developers{"Jessica" , 3500})
+    Salary := make(map[string]int)
+    Salary["Rajesh"] = 12000
+    Salary["Pavan"] = 15000
+    Salary["Gopi"] = 9000
+    fmt.Println("Salaries map contents:",Salary)
+   
+    fmt.Println(evennums)
+    fmt.Println("4th Element in array is:",evennums[3])
+        
+ }
 
-                
